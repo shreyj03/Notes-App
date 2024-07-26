d4284964663c0589a6f74d71c78906d059e7eaf5
shreyj03 <jainshrey2004@gmail.com>
2024-07-26 17:27:57 +0530

    minor error fixed in url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,8 +4,8 @@ import { ACCESS_TOKEN } from "./constants";
 const apiUrl = "/choreo-apis/notes-app/backend/v1";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL}) 
-
+  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
+});
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem(ACCESS_TOKEN);
